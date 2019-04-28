8f3927eba7ca86c0c361323c2b350d021d647414
Pei丶Code <aishiklpz@163.com>
2019-04-28 18:32:25 +0800

    初始化

---
+++ Example/Pods/Target Support Files/PSPhotoBrowser/PSPhotoBrowser-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PSPhotoBrowser : NSObject
+@end
+@implementation PodsDummy_PSPhotoBrowser
+@end
---
+++ Example/Pods/Target Support Files/Pods-PSPhotoBrowser_Example/Pods-PSPhotoBrowser_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PSPhotoBrowser_Example : NSObject
+@end
+@implementation PodsDummy_Pods_PSPhotoBrowser_Example
+@end
